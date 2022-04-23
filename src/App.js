4d3c9a27d5d0694b27f503a60f4962804d950257
Tushar Singh <tusharsingh44@gmail.com>
2022-04-23 23:19:29 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Tushar Singh</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/handlers/index.js
@@ -0,0 +1,9 @@
+import Seo from './seo';
+import Storage from './storage';
+import WebServices from './web-services';
+
+export default {
+  Seo,
+  Storage,
+  WebServices,
+};
